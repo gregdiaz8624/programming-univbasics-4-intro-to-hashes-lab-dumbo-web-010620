1a83447d329fcdd82709d1eaae8638f66bdd08ad
Gregory Diaz <gregdiaz8624@yahoo.com>
2019-12-30 21:52:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,22 @@
 def new_hash
-  # return an empty hash
+  return {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  {
+  name: "Gregory"
+} # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  return {
+  name: 'Grace Hopper'
+}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
-def id_generator
+def id_generator 
+ return {id: 30}
   # return a hash with a key :id assigned to positive integer
 end
 
